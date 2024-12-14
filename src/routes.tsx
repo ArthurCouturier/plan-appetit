c18e7c06227011b31fad5cbdfbc3ac4572a79120
CoutCout <72403468+ArthurCouturier@users.noreply.github.com>
2024-12-14 15:31:49 +0100

    fix: :fire: removed home page & about page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
 import Planning from "./pages/Planning";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/about",
-        element: <About />,
-    },
-    {
-        path: "/planning",
         element: <Planning />,
     },
 ]);

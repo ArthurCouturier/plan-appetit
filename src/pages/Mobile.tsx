40eb374abbc6de3bc809d147d29bb47c4401cc92
theoxnt <tnouet1306@gmail.com>
2025-07-21 11:10:17 +0200

    first commit : premier design du header

--- src/pages/Mobile.tsx
+++ src/pages/Mobile.tsx
@@ -4,34 +4,19 @@ import fleche from "@/icons/icon-fleche.png";
 export default function Mobile() {
   return (
     <div className="">
-
-      <div className="absolute inset-x-0 gap-1 bottom-4 flex justify-between bg bg-blue-600 rounded-3xl">
-        <Button className="bg bg-blue-900 rounded-3xl border-0">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path fill-rule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+      <header className="absolute inset-x-0 top-0 border-0 rounded-b-3xl bg-blue-600 shadow-md flex justify-between items-center">
+        <Button className="bg bg-blue-900 py-2 px-4"> 
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="text-black size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
           </svg>
-          {/* Ajouter recette */}
         </Button>
-        <Button className="bg bg-blue-900 rounded-3xl border-0">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path d="M12 .75a8.25 8.25 0 0 0-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 0 0 .577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 0 1-.937-.171.75.75 0 1 1 .374-1.453 5.261 5.261 0 0 0 2.626 0 .75.75 0 1 1 .374 1.452 6.712 6.712 0 0 1-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 0 0 .577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0 0 12 .75Z" />
-            <path fill-rule="evenodd" d="M9.013 19.9a.75.75 0 0 1 .877-.597 11.319 11.319 0 0 0 4.22 0 .75.75 0 1 1 .28 1.473 12.819 12.819 0 0 1-4.78 0 .75.75 0 0 1-.597-.876ZM9.754 22.344a.75.75 0 0 1 .824-.668 13.682 13.682 0 0 0 2.844 0 .75.75 0 1 1 .156 1.492 15.156 15.156 0 0 1-3.156 0 .75.75 0 0 1-.668-.824Z" clip-rule="evenodd" />
+          <h1 className="text-xl font-bold">Plan'Appetit</h1>
+        <Button className="bg bg-blue-900 py-2 px-4"> 
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className="size-6" viewBox="0 0 48 48">
+            <path d="M 24 4 L 1.0371094 18.029297 L 1.9296875 19.488281 C 3.0816875 21.372281 5.5427344 21.965453 7.4277344 20.814453 L 24 10.689453 L 40.572266 20.814453 C 42.456266 21.965453 44.917313 21.372281 46.070312 19.488281 L 46.962891 18.029297 L 39 13.164062 L 39 4 L 35 4 C 33.9 4 33 4.9 33 6 L 33 9.4980469 L 24 4 z M 24 14.210938 L 8.9902344 23.380859 C 8.0802344 23.940859 7.05 24.269375 6 24.359375 L 6 43 L 18 43 L 18 29 C 18 27.34 19.34 26 21 26 L 30 26 L 30 43 L 36 43 C 39.31 43 42 40.31 42 37 L 42 24.359375 C 40.95 24.269375 39.919766 23.940859 39.009766 23.380859 L 24 14.210938 z"></path>
           </svg>
-          {/* Générer recette */}
         </Button>
-        <Button className="bg bg-blue-900 rounded-3xl border-0">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
-          </svg>
-          {/* Mes recettes */}
-          </Button>
-        <Button className="bg bg-blue-900 rounded-3xl border-0">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
-          </svg>
-          {/* Mon compte */}
-        </Button>
-      </div>
+      </header>
     </div>
   )
 }

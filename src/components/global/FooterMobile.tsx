eaed817f03590100046334a8c6cb28556f37a51b
theoxnt <tnouet1306@gmail.com>
2025-07-22 10:29:57 +0200

    footer animation v1

--- src/components/global/FooterMobile.tsx
+++ src/components/global/FooterMobile.tsx
@@ -1,13 +1,36 @@
 import { Button } from "@material-tailwind/react";
 import { UserCircleIcon, BookOpenIcon, LightBulbIcon, PlusIcon } from "@heroicons/react/24/solid"
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function FooterMobile() {
 
   const navigateTo = useNavigate();
+
+  const [lastScrollY, setLastScrollY] = useState(window.scrollY);
+
+  const [showFooter, setShowFooter] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY) {
+        setShowFooter(false);
+      } else {
+        setShowFooter(true);
+      }
+      setLastScrollY(currentScrollY);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll)};
+    }, [lastScrollY]);
   
   return (
-    <div className="absolute inset-x-0 gap-1 bottom-4 flex justify-between bg bg-blue-600 rounded-3xl">
+    <footer className={`absolute inset-x-0 gap-1 bottom-4 flex justify-between bg-blue-600 rounded-3xl transition-transform duration-2000 ${
+      showFooter ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
+    }`}>
         <Button className="bg-blue-900 rounded-3xl border-0" onClick={() => navigateTo("/recettes")}>
         <PlusIcon className="w-6 h-6 text-white"/>
         </Button>
@@ -20,6 +43,6 @@ export default function FooterMobile() {
         <Button className="bg-blue-900 rounded-3xl border-0" onClick={() => navigateTo("/login")}>
           <UserCircleIcon className="w-6 h-6 text-white" />
         </Button>
-      </div>
+      </footer>
   )
 }

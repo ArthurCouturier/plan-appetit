d6fcf5697cf165be7590e9ecd69bceb6924b6d4c
Arthur Couturier <arthur.couturier.2000@gmail.com>
2025-12-10 19:38:08 +0100

    deleted three js stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import { AuthProvider } from './components/authentication/AuthProvider';
-import { useGLTF } from '@react-three/drei';
 
 function App() {
   return (
@@ -12,10 +11,5 @@ function App() {
     </AuthProvider>
   );
 }
-
-// Préchargement des modèles 3D
-useGLTF.preload("/models/business-plan-lower.glb");
-useGLTF.preload("/models/Monitor.glb");
-useGLTF.preload("/models/Recipes.glb");
 
 export default App;
